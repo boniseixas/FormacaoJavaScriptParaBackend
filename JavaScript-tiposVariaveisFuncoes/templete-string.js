25ddbf7e8d8bc490befd3f75b4ed495ab9ef42d2
boniseixas <bonisousa73@gmail.com>
2022-06-14 21:11:01 -0300

    Métodos de concatenação

---
+++ JavaScript-tiposVariaveisFuncoes/templete-string.js
@@ -0,0 +1,11 @@
+const nome = "Boni";
+const idade = 2022-1973;
+const cidadeNascimento = "Óbidos";
+
+// concatenando com o operador de adição (+)
+// const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci em " + cidadeNascimento;
+
+// concatenando com o templete de dolar ($)
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacao);
